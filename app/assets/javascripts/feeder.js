298816f6dc8df7f9089dfb5dd1aa703458ea630a
dyg2104 <dyg2104@gmail.com>
2014-09-07 12:27:57 -0400

    Removes coffescript files

---
+++ app/assets/javascripts/feeder.js
@@ -0,0 +1,13 @@
+window.Feeder = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Feeder.initialize();
+});
